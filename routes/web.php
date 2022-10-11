f6c5587fd2c1f9f3bdb23abd563b7a1fda91b7e2
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2022-10-10 21:03:04 -0300

    Add method POST register

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('principal');
 });
 
 Route::get('/crear-cuenta', [RegisterController::class, 'index']);
+Route::post('/crear-cuenta', [RegisterController::class, 'store']);
 
 
 
